8c95426e0ceebaf821c92e710f84af1ad8acffbd
youcef.ouarhlent <youcef.ouarhlent@laplateforme.io>
2023-11-20 14:41:47 +0100

    On écrit un script qui détermine si une chaîne contient ou non le caractère « e ».

---
+++ jour01/pour_aller_plus_loin/main.py
@@ -0,0 +1,7 @@
+chaine_de_caractère = input("Entrez une chaîne de caractères : ")
+
+
+if 'e' in chaine_de_caractère:
+    print("La chaîne de caractères contient le caractère 'e'.")
+else:
+    print("La chaîne de caractères ne contient pas le caractère 'e'.")
