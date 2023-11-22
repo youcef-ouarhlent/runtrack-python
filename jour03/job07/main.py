5633c111742ef7b2897852d94294bf82fce57b51
youcef.ouarhlent <youcef.ouarhlent@laplateforme.io>
2023-11-22 11:41:23 +0100

    Création d'une fonction qui prend en paramètre une String nommée 'langage'.

---
+++ jour03/job07/main.py
@@ -0,0 +1,16 @@
+def différents_langage(langage):
+    if langage == "JavaScript" :
+        print("tu es développeur web")
+    elif langage == "python" :
+        print("tu es développeur IA")
+    elif langage == "java" :
+        print("tu es développeur logiciel")
+    elif langage == "reactjs" :
+        print("tu es développeur mobile")
+    else :
+        print("un jour, je serai le meilleur développeur...")      
+différents_langage("JavaScript")   
+différents_langage("python") 
+différents_langage("java") 
+différents_langage("reactjs") 
+différents_langage("html")      
