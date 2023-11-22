3bb9accdffc806fe669c772c90907b35413577b3
youcef.ouarhlent <youcef.ouarhlent@laplateforme.io>
2023-11-22 11:26:59 +0100

    Création d'une fonction qui prend en paramètre un nombre nommé 'nombre'.

---
+++ jour03/job06/main.py
@@ -0,0 +1,14 @@
+def signe_nombre(nombre):
+    if nombre > 0:
+        print("positif")
+    elif nombre < 0 :
+        print("négatif")
+    else :
+        nombre ==0
+        print("Egal")       
+signe_nombre(9)
+signe_nombre(-9)
+signe_nombre(0)        
+
+
+    
