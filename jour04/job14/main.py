77f10a669fa372f5ea4a38fdb0c3cc130c06406a
youcef.ouarhlent <youcef.ouarhlent@laplateforme.io>
2023-11-23 18:53:23 +0100

    un programme contenant une fonction nommée 'my_long_word'. Cette fonction doit prendre deux paramètres, un chiffre entier et une chaîne de caractère.

--- jour04/job14/main.py
+++ jour04/job14/main.py
@@ -1,15 +1,15 @@
-def my_long_word(nombre,  chaine_de_caractere):
-    mots =  chaine_de_caractere.split()
+def my_long_word(nombre,chaîne_de_caractère):
+    mots = chaîne_de_caractère.split()
     resultat = ""
+
     for mot in mots:
-        mot_length = 0
-        for char in mot:
-            if char.isalpha():  
-                mot_length += 1
+        mot_length = 0 
+        for char in mot: 
+            mot_length += 1 
         
         if mot_length > nombre:
             resultat += mot + " "
-    return resultat.strip()  
 
+    return resultat.strip()
 output = my_long_word(3, "La peur est le chemin vers le côté obscur, la peur mène à la colère, la colère mène à la haine, la haine mène à la souffrance")
 print("Output:", output)
