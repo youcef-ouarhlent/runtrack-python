fca7b0546fb83c177262fdf835aecc38bd274f27
youcef.ouarhlent <youcef.ouarhlent@laplateforme.io>
2023-11-23 19:12:54 +0100

    Un programme qui arrondi les nombres de la liste et qui trie en ordre croissant

--- jour04/job15/main.py
+++ jour04/job15/main.py
@@ -1,17 +1,25 @@
 nombres = [22.4, 4.0, 16.22, 9.10, 11.00, 12.22, 14.20, 5.20, 17.50]
+
 def arrondir(nombre):
-    partie_entiere = int(nombre)
-    partie_decimale = nombre - partie_entiere
-    if partie_decimale >= 0.5:
-        return partie_entiere + 1
-    else:
-        return partie_entiere
+    partie_entiere = 0
+    while partie_entiere <= nombre:
+        partie_entiere += 1
+    return partie_entiere - 1
 
 liste_arrondie = [arrondir(nombre) for nombre in nombres]
-taille_liste_nombre = len(liste_arrondie)
-for i in range(taille_liste_nombre):
-    for j in range(0, taille_liste_nombre - i - 1):
+
+taille_liste_nombre = 0
+for elem in liste_arrondie:
+    taille_liste_nombre += 1
+
+i = 0
+while i < taille_liste_nombre:
+    j = 0
+    while j < taille_liste_nombre - i - 1:
         if liste_arrondie[j] > liste_arrondie[j + 1]:
             liste_arrondie[j], liste_arrondie[j + 1] = liste_arrondie[j + 1], liste_arrondie[j]
             nombres[j], nombres[j + 1] = nombres[j + 1], nombres[j]
-print("Liste arrondie de nombres qui est triée en ordre croissant : ", nombres)
+        j += 1
+    i += 1
+
+print("Liste arrondie de nombres triée en ordre croissant :", nombres)
