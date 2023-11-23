572a3659d36afe7344f840df41ce2e8387175cf7
youcef.ouarhlent <youcef.ouarhlent@laplateforme.io>
2023-11-23 19:10:23 +0100

    un programme qui trie dans l’ordre croissant une liste passée .

--- jour04/job12/main.py
+++ jour04/job12/main.py
@@ -1,3 +1,25 @@
+def count_elements(array):
+    count = 0
+    for _ in array:
+        count += 1
+    return count
+
+def tri_insertion(nombres):
+    i = 1
+    array_length = count_elements(nombres)
+    while i < array_length:
+        valeur_actuelle = nombres[i]
+        position = i
+
+        while position > 0:
+            if nombres[position - 1] > valeur_actuelle:
+                nombres[position] = nombres[position - 1]
+                position -= 1
+            else:
+                break
+
+        nombres[position] = valeur_actuelle
+        i += 1
 nombres = [3, 5, 4, 2, 1]
-nombres.sort()
+tri_insertion(nombres)
 print(nombres)
